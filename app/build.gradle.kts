4cc435db72dceaa9323231944ded0f77a6e01436
SAVASHUB <103499835+SAVASHUB@users.noreply.github.com>
2024-12-11 18:15:55 +0100

    Firebase, Authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -41,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation(libs.firebase.auth)
     val nav_version = "2.8.3"
 
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/example/repmax/AuthViewModel.kt
+++ app/src/main/java/com/example/repmax/AuthViewModel.kt
@@ -1,6 +1,33 @@
 package com.example.repmax
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel : ViewModel() {
+
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    private  val _authState = MutableLiveData<AuthState>()
+    val authState : LiveData<AuthState> = _authState
+
+    init {
+        checkAuthState()
+    }
+
+    fun checkAuthState () {
+        if(auth.currentUser == null) {
+            _authState.value = AuthState.Unauthenticated
+        }else {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+}
+
+sealed class AuthState{
+    object Authenticated : AuthState()
+    object Unauthenticated : AuthState()
+    object LoadingAuth : AuthState()
+    data class Error( val message : String) : AuthState()
 }
